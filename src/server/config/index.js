aee1fb1957058349c8a8c212053820aba4910954
halsn <xuhalsn@gmail.com>
2017-08-10 14:54:42 +0800

    update

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,13 +1,15 @@
+const { API_HOST, API_PORT, DB_URI } = process.env
+
 module.exports = {
   web: {
     domain: 'localhost',
     port: 5000
   },
   api: {
-    domain: 'localhost',
-    port: 6000
+    domain: API_HOST || 'localhost',
+    port: API_PORT || 6000
   },
   db: {
-    URI: 'mongodb://localhost:27017'
+    URI: DB_URI || 'mongodb://localhost:27017'
   }
 }
